9d559f3df9ce017e1dccc678689a78e1c095195b
Emmanuel Flores <emmanuelclflores@gmail.com>
2021-12-22 22:14:16 -0800

    Add Landing, Experience, Projects, Contact page files; Add background images

--- src/App.js
+++ src/App.js
@@ -4,28 +4,96 @@ const App = () => {
   return (
     // Navbar / menu
     <div>
-      <div>
-        About
-        <ul>
-          <li>Picture</li>
-          <li>Bio</li>
-          <li>Github / LinkedIn / Email</li>
-          <li>Resume</li>
-        </ul>
+      <div
+        style={{
+          backgroundImage: `url(${"warm-dragon-landscape.jpg"})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          width: "100vw",
+          height: "100vh",
+        }}
+        className="justify-center items-center flex"
+      >
+        <Placeholder name="About" />
       </div>
-
-      <div>PROJECTS</div>
-      <ul>
-        <li>Picture</li>
-        <li>Bio</li>
-      </ul>
-      <div>EDUCATION</div>
-      <div>BLOG</div>
-      <div>CONTACTS</div>
-      <ul>
-        <li>Send Email to Me</li>
-      </ul>
+      <div
+        style={{
+          backgroundImage: `url(${"blue-mountain-landscape.jpg"})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          width: "100vw",
+          height: "100vh",
+        }}
+        className="justify-center items-center flex"
+      >
+        <Placeholder name="About" />
+      </div>
+      <div
+        style={{
+          backgroundImage: `url(${"lake-landscape.jpg"})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          width: "100vw",
+          height: "100vh",
+        }}
+        className="justify-center items-center flex"
+      >
+        <Placeholder name="About" />
+      </div>
     </div>
+
+    // <div>
+
+    //   <div>
+    //     About
+    //     <ul>
+    //       <li>Picture</li>
+    //       <li>Bio</li>
+    //       <li>Github / LinkedIn / Email</li>
+    //       <li>Resume</li>
+    //     </ul>
+    //   </div>
+
+    //   <div>PROJECTS</div>
+    //   <ul>
+    //     <li>Picture</li>
+    //     <li>Bio</li>
+    //   </ul>
+    //   <div>EDUCATION</div>
+    //   <div>BLOG</div>
+    //   <div>CONTACTS</div>
+    //   <ul>
+    //     <li>Send Email to Me</li>
+    //   </ul>
+    // </div>
+  );
+};
+
+const styles = {
+  container: {
+    backgroundImage: `url(${"warm-dragon-landscape.jpg"})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    width: "100vw",
+    height: "100vh",
+  },
+};
+
+const Section = (props) => {
+  const { backgroundImage, content } = props;
+  return;
+};
+
+const Placeholder = (props) => {
+  const { name } = props;
+  return (
+    <p className="bg-black rounded-xl text-white items-center px-5 py-5">
+      {props.name}
+    </p>
   );
 };
 
